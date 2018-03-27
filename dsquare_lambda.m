c5d9cc32f4365bd4994cf5da2fcde4bc963673f8
ickythumped <roy.gaurav9421@gmail.com>
2018-03-27 16:55:01 -0500

    Have to complete dsquare_lambda

---
+++ dsquare_lambda.m
@@ -0,0 +1,9 @@
+function [dsquare_lambda_mat] = dsquare_lambda(f_val, integ_f_val, ...
+    d_lambda_vec, dsquaref_mat, integ_df_vec, integsq_df_vec) 
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+expr1 = 1/(1 - integ_f_val);
+
+end
+
