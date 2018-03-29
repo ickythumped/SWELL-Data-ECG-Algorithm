3b2c45385a8e51f54d92755b246fa038f5df415d
ickythumped <roy.gaurav9421@gmail.com>
2018-03-29 01:21:52 -0500

    Reduced delta to 0.003

--- del_parts.m
+++ del_parts.m
@@ -4,7 +4,7 @@ function [J, delta, n] = del_parts(T, fs, N)
 
 %% Computing J, delta
 %Divide [0,T] into J equal parts
-J = floor(T/(fs/32)); 
+J = floor(T/(fs/256)); 
 
 %Length of each part
 delta_samples = T/J; 
