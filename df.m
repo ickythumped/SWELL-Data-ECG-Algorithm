2f3ca2fe2d5087f335b878cd10c6ebf584e9180a
ickythumped <roy.gaurav9421@gmail.com>
2018-03-29 20:59:22 -0500

    Changes du/dtheta
    Changes to du/dtheta in df(), sym_df(), dsquaref() and sym_dsquaref()

--- df.m
+++ df.m
@@ -5,8 +5,13 @@ df_vec = zeros(1, nparams+2);
 
 expr1 = (f_val*theta_p_plus1)./(mu_val^3);
 expr2 = (j*delta - u_val - mu_val);
+
 for i = 1:nparams+1
-    df_vec(i) = H(k-i+2)*expr1*expr2;
+    if(i == 1)
+        df_vec(1) = expr1*expr2;
+    else
+        df_vec(i) = H(k-i+2)*expr1*expr2;
+    end
 end
 
 expr3 = (-(expr2)^2)./(2*(mu_val^2)*(j*delta - u_val));
--- sym_df.m
+++ sym_df.m
@@ -9,7 +9,9 @@ expr2 = (t - u_val - mu_val);
 expr3 = (-(expr2).^2)./(2*(mu_val^2).*(t - u_val));
 expr4 = f_val./(2*theta_p_plus1);
 
-if (i~=nparams+2)
+if (i == 1)
+    sym_df_val = expr1.*expr2;
+elseif (i~=nparams+2)
     sym_df_val = H(k-i+2).*expr1.*expr2;
 else
     sym_df_val = f_val.*expr3 + expr4;
