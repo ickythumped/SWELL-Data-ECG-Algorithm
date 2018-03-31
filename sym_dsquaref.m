3aac6cd91e43be9d50a7a9c573880cd31b5b1767
ickythumped <roy.gaurav9421@gmail.com>
2018-03-30 21:47:43 -0500

    merge

--- sym_dsquaref.m
+++ sym_dsquaref.m
@@ -11,30 +11,13 @@ df_dmu = (f_val.*theta_p_plus1.*(expr1 - mu_val))./(mu_val.^3);
 dsquaref_dmusquare = theta_p_plus1.*(df_dmu.*expr2 + f_val.*expr3);
 
 %% reg3
-<<<<<<< HEAD
-expr4 = ((expr1 - u_val).^2)./(2.*(mu^2).*expr1);
-=======
 expr4 = ((expr1 - u_val).^2)./(2.*(mu_val^2).*expr1);
->>>>>>> All-dsquaref_reg's-in-dsquaref
 expr5 = df_vec(end) - f_val./theta_p_plus1;
 
 %% reg2
 expr6 = (f_val*(expr1 - mu_val))./mu_val^3;
 
 %%
-<<<<<<< HEAD
-if (a <= nparams+1 && b <= nparams+1) %reg1
-    dsquaref_reg_val = dsquaref_dmusquare.*H(k-a+2).*H(k-b+2);
-
-elseif (a == nparams+2 && b == nparams+2) %reg3
-        dsquaref_reg_val = (df_vec(end).*(-expr4))...
-            + ((1/(2.*theta_p_plus1)).*expr5);
-
-% Please check again
-else
-    dsquaref_reg_val = ((1/(2*theta_p_plus1))*df_vec(a))...
-        - (df_vec(a)*expr4) + (expr6*H(k-a+2));
-=======
 if (a == 1 && b == 1)
     sym_dsquaref_val = dsquaref_dmusquare;
     
@@ -66,8 +49,6 @@ elseif (a <= nparams+1 && b == nparams+2)
 elseif (a == nparams+2 && b <= nparams+1)
     sym_dsquaref_val = ((1/(2*theta_p_plus1))*df_vec(b))...
         - (df_vec(b)*expr4) + (expr6*H(k-b+2));
->>>>>>> All-dsquaref_reg's-in-dsquaref
 end
 
 end
-
